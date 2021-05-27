77ba03487cb577e5220a1144ffd064d6053967dc
zhongj <zhongj@tcfuture.tech>
2021-05-27 14:22:01 +0800

    example

---
+++ examples/src/index.js
@@ -0,0 +1,4 @@
+const { sum } = require("../../lib/index.js");
+
+console.log(sum);
+console.log(sum(1, 6));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-function sum() {
-	num1 = parseFloat(num1);
-	num2 = parseFloat(num2);
-	return num1 + num2;
+export function sum(num1, num2) {
+  const _num1 = parseFloat(num1);
+  const _num2 = parseFloat(num2);
+  return _num1 + _num2;
 }
 
-module.export == sum;
+// export default sum;
